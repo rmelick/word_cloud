955214e0e4b03cefe241525ce0cf616652ca0f9e
Russell Melick <russell.melick@kiwigrid.com>
2017-03-01 13:31:08 +0100

    test-emoji to help play with regexes

---
+++ examples/test-emoji.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env pythonw
+# -*- coding: utf-8 -*-
+import unicodedata
+import re
+import sys
+
+frog = u'🐸'
+print ("frog is")
+print(frog)
+
+fileLines = open("simple-emoji.txt").readlines()
+print("file lines are")
+print(fileLines)
+print("each line is")
+for emojiString in fileLines:
+    print(emojiString)
+
+text = "".join(fileLines)
+regexp = r"\w[\w']+"
+flags = (re.UNICODE if sys.version < '3' and type(text) is unicode
+                 else 0)
+print("combined text is")
+print(text)
+
+words = re.findall(regexp, text, flags)
+print("words are")
+print(words)
